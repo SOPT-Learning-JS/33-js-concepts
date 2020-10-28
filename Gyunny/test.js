5ac736cda3f886ab861007b31eb4eb713dead3f9
Gyunny <wjdrbs966@naver.com>
2020-10-29 02:11:52 +0900

    1. Call Stack add

---
+++ Gyunny/test.js
@@ -0,0 +1,13 @@
+var name = 'Gyuuny'; // (1)변수 선언 (6)변수 대입
+
+function wow(word) { // (2)변수 선언 (3)변수 대입
+  console.log(word + ' ' + name); // (11)
+}
+
+function say () {   // (4)변수 선언 (5)변수 대입
+  var name = 'Gyun'; // (8)
+  console.log(name); // (9)
+  wow('hello'); // (10)
+}
+
+say(); // (7)
