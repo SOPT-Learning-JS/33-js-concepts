d5468963bdb65d75a2ef5bc13e46c1034e73018f
Gyunny <wjdrbs966@naver.com>
2020-11-02 02:16:28 +0900

    callStack update

--- Gyunny/test.js
+++ Gyunny/test.js
@@ -1,13 +1,13 @@
-function foo() { 
-  throw new Error('에러가 발생했습니다'); 
+var x = 'xxx';
+
+function foo() {
+  var y = 'yyy';
+
+  function bar() {
+    var z = 'zzz';
+    console.log(x + y + z); // xxxyyyzzz
+  }
+  bar();
 }
 
-function bar() { 
-  throw new Error("이발"); 
-}
-
-function start() { 
-  bar(); 
-}
-
-start();
+foo();
