c398ef4d7a711a7ab2ad4589acb9d9afc5f1af39
Gyunny <wjdrbs966@naver.com>
2020-11-10 21:10:02 +0900

    오타제거

--- Gyunny/test.js
+++ Gyunny/test.js
@@ -1,4 +1,11 @@
-var person = {
-  name: 'Lee'
-};
+var str = 'string';
 
+// 문자열은 유사배열이다.
+for (var i = 0; i < str.length; i++) {
+  console.log(str[i]);
+}
+
+// 문자열을 변경할 수 없다.
+str[0] = 'S';
+console.log(str.toUpperCase());
+console.log(str); // string
