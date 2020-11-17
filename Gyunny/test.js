5736a66eb5814df496bcbb4ce238e15445d90e1b
Gyunny <wjdrbs966@naver.com>
2020-11-17 22:33:03 +0900

    3주차 add

--- Gyunny/test.js
+++ Gyunny/test.js
@@ -1,11 +1,5 @@
-var str = 'string';
+var o = { p: 42, q: true };
+var { p, q } = o;
 
-// 문자열은 유사배열이다.
-for (var i = 0; i < str.length; i++) {
-  console.log(str[i]);
-}
-
-// 문자열을 변경할 수 없다.
-str[0] = 'S';
-console.log(str.toUpperCase());
-console.log(str); // string
+console.log(p); // 42
+console.log(q); // true
